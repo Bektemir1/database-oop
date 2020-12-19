01e126cfea9d84a1eda82ff06583d4792b6bea4c
Bektemir1 <kudajberdievbektemir@gmail.com>
2020-12-19 14:02:57 +0600

    added project

---
+++ AtmManagementSys/src/main/java/com/mycompany/atmmanagementsys/Quotes.java
@@ -0,0 +1,52 @@
+
+package com.mycompany.atmmanagementsys;
+
+import java.util.Random;
+
+
+public class Quotes {
+    
+    String quote ;
+    
+    public String returnQuotes(){
+    
+    
+    Random ran = new Random();
+        int num = ran.nextInt(7);
+
+        switch (num) {
+
+            case 0:
+               quote  = "Money Management";
+                
+                break;
+            case 1:
+                quote  = "Money Management";
+                break;
+            case 2:
+                quote  = "Money Management";
+                break;
+            case 3:
+                quote  = "Money Management";
+                break;
+            case 4:
+                quote  = "Money Management";
+                break;
+            case 5:
+                quote  = "Money Management";
+                break;
+
+            case 6:
+                quote  = "Money Management";
+    
+    
+    
+    }
+    
+    
+    return quote;
+    
+    
+    }
+    
+}
